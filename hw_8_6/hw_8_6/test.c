151d69135e9965c2c63da2a1ee69a2a5a5941d2a
wukaishun <1254377530@qq.com>
2022-08-07 00:04:34 +0800

    8_6

--- hw_8_6/hw_8_6/test.c
+++ hw_8_6/hw_8_6/test.c
@@ -90,16 +90,19 @@ int main()
                 printf(" ");
             for (j = 0; j < i + 1; j++)
                 printf("*");
+            printf("\n");
         }
-        for (i = 0; i < i + 1; i++)
+        for (i = 0; i < n + 1; i++)
             printf("*");
+        printf("\n");
         for (i = 0; i < n; i++)
         {
             int j = 0;
             for (j = 0; j < 2 * (i + 1); j++)
                 printf(" ");
-            for (j = 0; j < n + 1 - i; j++)
+            for (j = 0; j < n - i; j++)
                 printf("*");
+            printf("\n");
         }
     }
     return 0;
