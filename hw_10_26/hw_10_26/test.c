ffc075b989e3aab2324ff797dd8209c9af6554d0
wukaishun <1254377530@qq.com>
2022-10-26 14:08:39 +0800

    原地移除数组中所有的元素val，要求时间复杂度为O(N)，空间复杂度为O(1)

---
+++ hw_10_26/hw_10_26/test.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//原地移除数组中所有的元素val，要求时间复杂度为O(N)，空间复杂度为O(1)
+#include <stdio.h>
+
+int removeElement(int* nums, int numsSize, int val)
+{
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < numsSize; i++)
+    {
+        if (nums[i] != val)
+        {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    return j;
+}
+
+int main()
+{
+    int arr[] = { 0,1,2,2,3,0,4,2 };
+    int sz = sizeof(arr) / sizeof(arr[0]);
+    int n = 2;
+    int i = 0;
+    int j=removeElement(arr, sz, n);
+    for (i = 0; i < j; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n%d\n", j);
+	return 0;
+}
