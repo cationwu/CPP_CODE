07231c7daedad824032d4ce8ae056345a4072715
wukaishun <1254377530@qq.com>
2022-11-15 13:23:57 +0800

    队列的复现

---
+++ Queue/Queue/test.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Queue.h"
+
+void TestQueue()
+{
+	Queue q;
+	QueueInit(&q);
+	QueuePush(&q, 1);
+	QueuePush(&q, 2);
+
+	printf("%d ", QueueFront(&q));
+	QueuePop(&q);
+
+	QueuePush(&q, 3);
+	QueuePush(&q, 4);
+
+	printf("%d\n", QueueSize(&q));
+	printf("%d\n", QueueEmpty(&q));
+	printf("%d\n", QueueFront(&q));
+	printf("%d\n", QueueBack(&q));
+
+	while (!QueueEmpty(&q))
+	{
+		printf("%d ", QueueFront(&q));
+		QueuePop(&q);
+	}
+	printf("\n");
+
+	printf("%d\n", QueueSize(&q));
+	printf("%d\n", QueueEmpty(&q));
+	//printf("%d\n", QueueFront(&q));
+	//printf("%d\n", QueueBack(&q));
+
+	QueueDestroy(&q);
+}
+
+int main()
+{
+	TestQueue();
+
+	return 0;
+}
